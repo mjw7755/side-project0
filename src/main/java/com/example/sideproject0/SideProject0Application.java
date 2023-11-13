0f7f8e1ca0102745814f3a530a3a239da082e052
woong <mjw77557@gmail.com>
2023-11-13 22:06:02 +0900

    Initial commit

---
+++ src/main/java/com/example/sideproject0/SideProject0Application.java
@@ -0,0 +1,13 @@
+package com.example.sideproject0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideProject0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideProject0Application.class, args);
+    }
+
+}
